8d9a184e8b919d460fc784b3907ea70383be3aa1
Axel Klenk <axel.klenk@gmail.com>
2025-01-13 19:01:57 +0100

    copy bare minimum from expkg and add dependent test class D

---
+++ R/AllClasses.R
@@ -0,0 +1,16 @@
+
+#' `D` class
+#'
+#' A test class, subclass of test class `A` via `C`.
+#'
+#' This class is part of a pair of classes for testing an issue with S4
+#' method dispatch.
+#'
+#' @importClassesFrom expkg A C
+#' 
+#' @aliases D
+#' @name D-class
+#' @rdname D-class
+#' @exportClass D
+setClass("D", contains=c("C"))
+
