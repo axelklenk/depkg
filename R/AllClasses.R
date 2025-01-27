7a5f3b3fe2ae9f42309b8f330c009a14863a32b7
Axel Klenk <axel.klenk@gmail.com>
2025-01-27 17:17:05 +0100

    further minimized package according to advice by Martin Maechler

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,19 +1,2 @@
-
-#' @import methods
-NULL
-
-#' `D` class
-#'
-#' A test class, subclass of test class `A` via `C`.
-#'
-#' This class is part of a pair of classes for testing an issue with S4
-#' method dispatch.
-#'
-#' @importClassesFrom expkg A C
-#' 
-#' @aliases D
-#' @name D-class
-#' @rdname D-class
-#' @exportClass D
 setClass("D", contains=c("C"))
 
